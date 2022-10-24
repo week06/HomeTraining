c079fb15ff516279db34007c3dd2c25982f38276
chunngamm <kjwharry@naver.com>
2022-10-24 22:34:17 +0900

    김장원커밋합니다

---
+++ src/main/java/com/example/hometraing/controller/response/MypageResponseDto.java
@@ -0,0 +1,23 @@
+//package com.example.hometraing.controller.response;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Builder
+//@Getter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//
+//public class MypageResponseDto {
+//    private Long id;
+//    private String title;
+//    private String nickname;
+////    private String image;
+////    private String video;
+//    private String CreatedAt;
+//    private String ModifiedAt;
+//    private String content;
+//
+//}
