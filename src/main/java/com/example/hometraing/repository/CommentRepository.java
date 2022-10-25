bfad6edbac08b41c13afc0c4474ad3ee1dab7604
ssoyeon59 <nebula059@naver.com>
2022-10-25 18:54:27 +0900

    댓글레포지토리 수정

--- src/main/java/com/example/hometraing/repository/CommentRepository.java
+++ src/main/java/com/example/hometraing/repository/CommentRepository.java
@@ -2,16 +2,15 @@ package com.example.hometraing.repository;
 
 import com.example.hometraing.domain.Board;
 import com.example.hometraing.domain.Comment;
-import com.example.hometraing.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CommentRepository  extends JpaRepository<Comment, String>{
+public interface CommentRepository  extends JpaRepository<Comment, Long>{
     Optional<Comment> findById(Long id);
 
     List<Comment> findAllByBoardId(Long boardId);
-//    Comment findByBoardIdAndCommentId(Long postingId, Long commentId);
+//    Comment findByBoardIdAndCommentId(Long boardId, Long commentId);
 
 }
