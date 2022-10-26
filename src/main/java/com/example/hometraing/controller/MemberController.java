00f95ebe3e8075309bf408a4762861e4c7f26ae1
ssoyeon59 <nebula059@naver.com>
2022-10-26 15:43:08 +0900

    Changes by dddd

--- src/main/java/com/example/hometraing/controller/MemberController.java
+++ src/main/java/com/example/hometraing/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
--- src/main/java/com/example/hometraing/service/MemberService.java
+++ src/main/java/com/example/hometraing/service/MemberService.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.Optional;
